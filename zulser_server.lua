e1d769fa140ecd1a7a5dc4f0a8da89bc5e697263
RepeatPan <quit@users.noreply.github.com>
2015-05-17 10:53:24 +0200

    Added version information from smodify

--- zulser_server.lua
+++ zulser_server.lua
@@ -19,6 +19,17 @@ local function create_service(name)
 end
 
 radiant.events.listen(zulser, 'radiant:init', function()
+  -- Dump the version into our logfile... or something.
+  local success, json = pcall(radiant.resources.load_json, 'zulser/smodify.json')
+  local version_string
+  if success then
+    version_string = string.format('%s/%s; release %q based on commit %s (%q) on %s', json.user, json.repository, json.release_name, json.sha, json.release_tag, json.commit_date_utc)
+  else
+    version_string = 'unknown version, probably git/dev'
+  end
+  
+  radiant.log.write('zulser', 0, 'Using %s', version_string)
+  
   zulser._sv = zulser.__saved_variables:get_data()
   create_service('automation')
 end)
